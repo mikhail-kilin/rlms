8c26450c0a20c1d7fc9239ca5090c82b6a3dfcd7
Салават <slwtk@mail.ru>
2020-02-27 17:06:03 +0300

    Added status + validations

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,3 +1,4 @@
 class Course < ApplicationRecord
   belongs_to :teacher
+  validates :status, presence: true, inclusion: { in: %w(draft active archived) }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_20_133045) do
+ActiveRecord::Schema.define(version: 2020_02_27_134859) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,8 @@ ActiveRecord::Schema.define(version: 2020_02_20_133045) do
     t.integer "teacher_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "status", default: "draft"
+    t.index ["status"], name: "index_courses_on_status"
     t.index ["teacher_id"], name: "index_courses_on_teacher_id"
   end
 
